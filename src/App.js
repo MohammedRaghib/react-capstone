89996ce009def4db76c238e226f06c01eedf4f2d
DAKAMU254 <muchaikamau137@gmail.com>
2024-11-05 13:37:31 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { UserProvider } from './UserContext';
 import RoleBasedView from './RoleBasedView';
 import ProgressBar from './ProgressBar';
-
+import Components from './Components/AdminPanel'
 const App = () => {
     return (
         <UserProvider>
