da6f0d49c9f992d7268221a4ecd1e38698f05720
MohammedRaghib <mohammedmraghib@gmail.com>
2024-11-06 11:56:02 +0300

    removed pop up message div

--- src/App.js
+++ src/App.js
@@ -10,17 +10,6 @@ function App() {
 
     return (
         <>
-        <div style={{ textAlign: 'center', marginTop: '50px' }}>
-            <h1>Welcome to Simple App</h1>
-            <button onClick={toggleMessage} style={{ padding: '10px', fontSize: '16px' }}>
-                {showMessage ? 'Hide' : 'Show'} Message
-            </button>
-            {showMessage && (
-                <p style={{ marginTop: '20px', fontSize: '18px' }}>
-                    This is a simple message displayed by toggling the button!
-                </p>
-            )}
-        </div>
         </>
     );
 }
